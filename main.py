08e08f39e26e8a671316334d8e87843ab61d65f0
Anna Radjenovic <qo19103@bristol.ac.uk>
2024-01-23 11:01:50 +0000

    scipy root function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
 from solvers import solvers
+from scipy import optimize
 
 def f(x):
-    return np.cos(x) - x
+    return np.cos(x)-x
 
-print(solvers(1,f,1e-15))
+print(solvers(1,f,1e-5))
+
+sols = optimize.root(f, 0)
+print(sols.x)
